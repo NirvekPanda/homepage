8d164f740e8cfbb5293d776d78658fda26e69427
NirvekPanda <nirvekpandey@gmail.com>
2025-12-28 12:46:49 -0800

    improved contact me styling

--- src/app/contact/messageSent.js
+++ src/app/contact/messageSent.js
@@ -1,12 +1,11 @@
 export default function MessageSent() {
   return (
     <div
-      id="toast-simple"
-      className="flex items-center justify-center w-full max-w-xs p-4 space-x-4 text-black dark:text-white bg-white/25 dark:bg-black/25 backdrop-blur-sm rounded-lg shadow-lg border border-white/30 dark:border-gray-700/30 transition-all duration-200"
+      className="flex items-center justify-center py-2 gap-2"
       role="alert"
     >
       <svg
-        className="w-5 h-5 text-blue-600 dark:text-blue-400 rotate-45"
+        className="w-5 h-5 text-black dark:text-white flex-shrink-0 transition-colors duration-200 rotate-45"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -20,9 +19,9 @@ export default function MessageSent() {
           d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"
         />
       </svg>
-      <div className="text-sm font-medium text-center flex-grow">
+      <span className="text-black dark:text-white text-base font-medium transition-all duration-200 text-center leading-tight">
         Message sent successfully.
-      </div>
+      </span>
     </div>
   );
 }
